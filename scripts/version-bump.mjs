53e735d4925ac44cbc3140ddd95531c9ae43006c
Makary Sharoyan <yashhig@gmail.com>
2024-03-06 22:32:40 +0100

    updated varsion-bump script

--- scripts/version-bump.mjs
+++ scripts/version-bump.mjs
@@ -6,17 +6,30 @@ if (!targetVersion) {
 	process.exit(1);
 }
 
+const packageLockJsonPath = "package-lock.json"
+const packageLockJson = JSON.parse(readFileSync(packageLockJsonPath, "utf8"));
+const apiVersion = packageLockJson["packages"]["node_modules/obsidian"]?.["version"];
+if (typeof apiVersion !== 'string' || !apiVersion.match(/^\d+\.\d+\.\d+$/)) {
+	console.error(`Could not determine current api version, found: "${apiVersion}"`);
+	process.exit(1);
+}
+
 const packageJsonPath = "package.json"
 const packageJson = JSON.parse(readFileSync(packageJsonPath, "utf8"));
 packageJson.version = targetVersion;
 writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, "\t"));
 
+
+
 const manifestPath = "manifest.json"
 const manifest = JSON.parse(readFileSync(manifestPath, "utf8"));
 manifest.version = targetVersion;
+manifest.minAppVersion = apiVersion;
 writeFileSync(manifestPath, JSON.stringify(manifest, null, "\t"));
+
+
 
 const versionsPath = "versions.json";
 const versions = JSON.parse(readFileSync(versionsPath, "utf8"));
-versions[targetVersion] = manifest.minAppVersion;
+versions[targetVersion] = apiVersion;
 writeFileSync(versionsPath, JSON.stringify(versions, null, "\t"));
